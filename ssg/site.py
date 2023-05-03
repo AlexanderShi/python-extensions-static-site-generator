1923581041c74b58629d5379564dbeebe44e2032
Rui Shi <rui.shi@sas.com>
2023-05-03 17:55:44 -0400

    6

--- ssg/site.py
+++ ssg/site.py
@@ -9,16 +9,13 @@ class Site:
         self.source = Path(source)
         self.dest = Path(dest)
         self.parsers = parsers or []
-
     def create_dir(self, path):
         directory = self.dest / path.relative_to(self.source)
         directory.mkdir(parents=True, exist_ok=True)
-
     def load_parser(self, ext):
         for parser in self.parsers:
             if parser.valid_file_ext(ext):
                 return parser
-
     def run_parser(self, path):
         parser = self.load_parser(path.suffix)
         if parser is not None:
@@ -35,4 +32,7 @@ class Site:
             if path.is_dir():
                 self.create_dir(path)
             elif path.is_file():
-                self.run_parser(path)    
+                self.run_parser(path)
+    @staticmethod
+    def error(message):
+        sys.stderr.write("\x1b[1;31m{}\n".format(message))
